6b350a3223f99fc5d9a71dfbea942f6460212719
Hitesh <hiteshmistri08@gmail.com>
2021-02-23 13:16:11 +0530

    - Configure project folder - Add login screen

---
+++ Assessment/Assessment/Architecture/Login/Controller/LoginVC.swift
@@ -0,0 +1,40 @@
+//
+//  LoginVC.swift
+//  Assessment
+//
+//  Created by Hitesh on 23/02/21.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func onBtnLoginAction(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
